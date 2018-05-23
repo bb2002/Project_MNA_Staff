1776d41d7054d21ce0621ee5dffb046fc9c52dd8
5252b <5252bb@daum.net>
2018-05-24 00:11:44 +0900

    Initial commit

---
+++ app/src/main/java/kr/saintdev/mna_staff/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.saintdev.mna_staff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
