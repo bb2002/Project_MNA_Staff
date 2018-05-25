751767ee2b3357ffd267196980cc90eafde0815e
5252b <5252bb@daum.net>
2018-05-26 00:02:51 +0900

    05.26 00:01 직원 앱 공용 라이브러리 생성을 완료했습니다.

--- app/src/main/java/kr/saintdev/mnastaff/views/activitys/MainActivity.java
+++ app/src/main/java/kr/saintdev/mnastaff/views/activitys/MainActivity.java
@@ -1,7 +1,9 @@
-package kr.saintdev.mnastaff;
+package kr.saintdev.mnastaff.views.activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import kr.saintdev.mnastaff.R;
 
 public class MainActivity extends AppCompatActivity {
 
